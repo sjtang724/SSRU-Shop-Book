29b11e0606afc085a322922761c3d47340d9f096
winita <winita1994@hotmail.com>
2016-05-31 10:15:39 +0700

    Upload Finish SQL

--- app/src/main/java/ssru/winitawilawan/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/winitawilawan/ssrushopbook/SignUpActivity.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวแปร
     private EditText nameEditText, surnamEditText, userEditText, passwordEditText;
-    private String nameString, surnameString, userString,passwordString;
-
+    private String nameString, surnameString, userString, passwordString;
+    private static final String urlUpload = "http://swiftcodingthai.com/ssru/add_user_nam.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +52,36 @@ public class SignUpActivity extends AppCompatActivity {
         } else {
 
             //NO Space
+            uploadNewUser();
         }
 
     } //clickSign
+
+    private void uploadNewUser() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name" ,nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/ssru/add_user_nam.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+    } // uploadNewUser
 
 } // Main Class
