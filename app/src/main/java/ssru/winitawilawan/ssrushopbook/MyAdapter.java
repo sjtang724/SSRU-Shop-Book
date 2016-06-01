19a9cfbe645d45b51f56263c5e2062befc27fb1a
winita <winita1994@hotmail.com>
2016-06-01 14:43:56 +0700

    confirm Order

--- app/src/main/java/ssru/winitawilawan/ssrushopbook/MyAdapter.java
+++ app/src/main/java/ssru/winitawilawan/ssrushopbook/MyAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Pc on 1/6/2559.
@@ -54,6 +57,10 @@ public class MyAdapter extends BaseAdapter {
 
         TextView priceTextView = (TextView) view1.findViewById(R.id.textView11);
         priceTextView.setText(priceStrings[i] + "THB.");
+
+        ImageView imageView = (ImageView) view1.findViewById(R.id.imageView2);
+        Picasso.with(context).load(coverStrings[i]).resize(120,150).into(imageView);
+
 
         return view1;
     }
--- app/src/main/java/ssru/winitawilawan/ssrushopbook/ProductListView.java
+++ app/src/main/java/ssru/winitawilawan/ssrushopbook/ProductListView.java
@@ -1,9 +1,24 @@
 package ssru.winitawilawan.ssrushopbook;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ProductListView extends AppCompatActivity {
 
@@ -11,7 +26,9 @@ public class ProductListView extends AppCompatActivity {
 
     private TextView nameTextView, surnameTextView, moneyTextView;
     private ListView listView;
-    private String[] loginStrings;
+    private String[] loginStrings, nameStrings, priceStrings,
+            coverStrings, eBookStrings;
+    private String urlJSON = "http://swiftcodingthai.com/ssru/get_product.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +49,142 @@ public class ProductListView extends AppCompatActivity {
         surnameTextView.setText(loginStrings[2]);
         moneyTextView.setText(loginStrings[5] + "THB.");
 
+        SynchronizeProduct synchronizeProduct = new SynchronizeProduct(this, urlJSON);
+        synchronizeProduct.execute();
 
     } //main method
+
+    private class SynchronizeProduct extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private String urlString;
+        private ProgressDialog progressDialog;
+
+        public SynchronizeProduct(Context context,
+                                  String urlString) {
+            this.context = context;
+            this.urlString = urlString;
+
+        } //Constructor
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(context, "Load product",
+                    "Load Product Process ...");
+
+
+        } //OnPre
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("1JuneV1", "doIn e ==> " + e.toString());
+                return null;
+            }
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if (s.length() != 0) {
+                progressDialog.dismiss();
+                Log.d("1JuneV1", "s ==> " + s);
+            }
+
+            try {
+
+
+                JSONArray jsonArray = new JSONArray(s);
+                nameStrings = new String[jsonArray.length()];
+                priceStrings = new String[jsonArray.length()];
+                coverStrings = new String[jsonArray.length()];
+                eBookStrings = new String[jsonArray.length()];
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    nameStrings[i] = jsonObject.getString("Name");
+                    priceStrings[i] = jsonObject.getString("Price");
+                    coverStrings[i] = jsonObject.getString("Cover");
+                    eBookStrings[i] = jsonObject.getString("Ebook");
+
+                }//for
+
+                MyAdapter myAdapter = new MyAdapter(context, coverStrings,
+                        nameStrings, priceStrings);
+                listView.setAdapter(myAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+
+                        if (checkMoney(priceStrings[i])) {
+
+                            confirmDialog(nameStrings[i], priceStrings[i]);
+
+                        } else {
+
+                            MyAlert myAlert = new MyAlert();
+                            myAlert.myDialog(context, "เงินไม่พอ", "กรุณาเลือกเล่มใหม่ เงินไม่พอ");
+
+                        }
+
+                    }
+                });
+
+            } catch (Exception e) {
+                Log.d("1JuneV2", "onPost e ==> " + toString());
+            }
+        }//onPost
+
+        private boolean checkMoney(String priceString) {
+
+            int intMyMoney = Integer.parseInt(loginStrings[5]);
+            int intPrice = Integer.parseInt(priceString);
+
+            if (intMyMoney >= intPrice) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }//Syn class
+
+    private void confirmDialog(String nameString, String priceString) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.icon_myaccount);
+        builder.setCancelable(false);
+        builder.setTitle("Confirm Order");
+        builder.setMessage(nameString + "Price " + priceString + "THB. " + "\n" + "? ");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+            }
+        });
+        builder.setPositiveButton("order", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }//confirm
 }//main class
